87978ef5956a87e6039ad9a2224e6ffc4baedf7a
heejee-yun <heejee.yun@gmail.com>
2022-08-07 17:38:01 +0900

    memo

--- Jun-Dec/W6.java
+++ Jun-Dec/W6.java
@@ -6,6 +6,18 @@ public class test {
 	
 		// ++ 앞과 뒤에 넣는 것에 따른 차이점 학습하기 
 		// 주의 사항 :  함수는 최대한 쪼개서 많이 만들기 한번에 다 넣어서 하지 않기
+		// from FreeCodeCamp.org 
+		
+		
+		//Principal:  100000
+		//Annual Interest Rate : 3.92 
+		//Period (years) : 30
+		//Mortgage : $472.81
+		//Process finished with exit code 
+		
+		Math.pow
+		
+		
 	
 	
 	}
