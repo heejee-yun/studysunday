f0d15ed20204eb87965db194ac58be33b0c0a027
heejee-yun <heejee.yun@gmail.com>
2022-08-11 16:30:50 +0900

    memo

--- Jun-Dec/W6.java
+++ Jun-Dec/W6.java
@@ -18,6 +18,17 @@ public class test {
 		Math.pow
 		
 		
+		# marks1.py
+		marks = [90, 25, 67, 45, 80]
+
+		number = 0 
+		for mark in marks: 
+		    number = number +1 
+		    if mark >= 60: 
+		        print("%d번 학생은 합격입니다." % number)
+		    else: 
+		        print("%d번 학생은 불합격입니다." % number)
+		
 	
 	
 	}
