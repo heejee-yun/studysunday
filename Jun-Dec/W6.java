e756391de569fb2479d4683c0344db488ef33ffe
heejee-yun <108048766+heejee-yun@users.noreply.github.com>
2022-08-14 23:37:19 +0900

    Update W6.java

--- Jun-Dec/W6.java
+++ Jun-Dec/W6.java
@@ -49,7 +49,53 @@ public class test {
 			case 12: System.out.println(month + "월은 겨울 입니다");          
 			default: System.out.println("1~12의 값을 입력해 주세요");
 		} 	}
-				
+			
+		public class SwitchTest {
+	public static void main(String[] args) {
+		// 언어 모드 선택 프로그램
+		// 사용자로부터 선택을 받아
+		// 해당 언어 모드로 바꾸었다는 메시지 출력하기
+		/*
+		 * 1. 파이썬
+		 * 2. 자바
+		 * 3. C
+		 * 4. JS
+		 */
+		
+		// 메시지 선언
+		String msg = "사용하고 싶은 언어를 고르세요.\n1.파이썬\n2.자바\n3.C\n4.JS";
+		String pythonMsg="파이썬입니다.";
+		String javaMsg = "자바입니다.";
+		String cMsg = "C입니다.";
+		String jsMsg = "JS입니다.";
+		String errMsg = "다시 시도해주세요.";
+		// 변수 선언
+		Scanner sc = new Scanner(System.in);
+		int choice=0;
+		
+		// 메뉴 시작
+		// 안내 메시지 출력 및 입력 받기
+		System.out.println(msg);
+		choice=sc.nextInt();
+		// 모드 전환 메시지 출력하기
+		switch(choice) {
+		case 1:
+			System.out.println(pythonMsg);
+			break;
+		case 2:
+			System.out.println(javaMsg);
+			break;
+		case 3:
+			System.out.println(cMsg);
+			break;
+		case 4:
+			System.out.println(jsMsg);
+			break;
+		default:
+			System.out.println(errMsg);
+		}
+	}
+}
 				
 				
 	
